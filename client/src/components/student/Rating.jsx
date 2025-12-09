9cc03f6de6c50510cd02aba2f529338caeaddef4
Yashsuryan <suryanyash43@gmail.com>
2025-12-10 01:01:17 +0530

    feat(player): Course Sturctue, Video player

--- client/src/components/student/Rating.jsx
+++ client/src/components/student/Rating.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Rating = () => {
+const Rating = ({iniitialRating, onRate}) => {
+
+    const [rating, setRating] = useState(iniitialRating || 0)
+
+    const handleRating = (value) => {
+        setRating(value);
+        if(onRate) onRate(value)
+    }
+
+    useEffect(()=>{
+        if(iniitialRating){
+            setRating(iniitialRating)
+        }
+    },[iniitialRating]);
+
   return (
     <div>
-        <h1>Rating</h1>
+        {Array.from({length: 5}, (_, index)=>{
+            const starValue = index + 1;
+            return (
+                <span key={index} className={`text-xl sm:text-2xl cursor-pointer transition-colors ${starValue <= rating ? 'text-yellow-500' : 'text-gray-400'}`} onClick={()=> handleRating(starValue)}>
+                    &#9733;
+                </span>
+            )
+        })}
     </div>
   )
 }
