901753a18f3fe8b3fed884b703bacc3713a37421
Yashsuryan <suryanyash43@gmail.com>
2025-12-14 01:04:25 +0530

    feat(Educator.jsx): Educator Navbar

--- client/src/components/educator/Navbar.jsx
+++ client/src/components/educator/Navbar.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { assets, dummyEducatorData } from "../../assets/assets";
+import { UserButton, useUser } from "@clerk/clerk-react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const educatorData = dummyEducatorData;
+  const user = useUser();
   return (
-    <div>
-        <h1>Edu Navbar</h1>
+    <div className="flex items-center justify-betwwen px-4 md:px-8 border-b border-gray-500 py-3">
+      <Link to={'/'}>
+        <img src={assets.logo2} alt="logo" className="w-28 lg:w-32" />
+      </Link>
+      <div className="flex items-center gap-5 text-gray-500 relative">
+        <p>Hi! {user ? user.fullName : 'Developers'}</p>
+        {user ? <UserButton /> : <img className="ma-w-8" src={assets.profile_img}/>}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
